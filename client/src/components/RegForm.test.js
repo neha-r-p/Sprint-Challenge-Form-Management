09cfe6245b3edd4ac2b5c78e6a5123728b8d688d
Neha Patel <neha.r.p@gmail.com>
2019-08-09 09:28:24 -0700

    trying to set up test for submit button...moving on for now

--- client/src/components/RegForm.test.js
+++ client/src/components/RegForm.test.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import FormikRegForm from './RegForm';
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import FormikRegForm from "./RegForm";
 
-describe('<FormikRegForm />', () => {
-    it('renders without crashing', () => {
-        render(<FormikRegForm />);
-       
-      });
-      
-})
+describe("<FormikRegForm />", () => {
+  it("renders without crashing", () => {
+    render(<FormikRegForm />);
+  });
+  it("submit button click", () => {
+    const { getByText } = render(<FormikRegForm />);
+    const submitBtn = getByText(/submit/i)
+  });
+});
