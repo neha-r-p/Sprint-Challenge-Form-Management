671853b74057d097a9f987a9e4a1552ad57ba6a3
Neha Patel <neha.r.p@gmail.com>
2019-08-09 09:33:23 -0700

    RegForm.js setting up mapPropsToValues

--- client/src/components/RegForm.js
+++ client/src/components/RegForm.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Form, Field, withFormik } from 'formik';
+import React from "react";
+import { Form, Field, withFormik } from "formik";
 
 const RegForm = () => {
-    return (
-        <>
-        <Form>
-            <Field type="text" name="username" placeholder="Username" />
-            <Field type="password" name="password" placeholder="Password" />
-            <button type="submit">Submit</button>
-        </Form>
-        </>
-    )
+  return (
+    <>
+      <Form>
+        <Field type="text" name="username" placeholder="Username" />
+        <Field type="password" name="password" placeholder="Password" />
+        <button type="submit">Submit</button>
+      </Form>
+    </>
+  );
 };
 
-const FormikRegForm = withFormik({})(RegForm);
+const FormikRegForm = withFormik({
+  mapPropsToValues: ({ username, password }) => {
+      return {
+          username: username || '',
+          password: password || ''
+      }
+  }
+})(RegForm);
 
 export default FormikRegForm;
